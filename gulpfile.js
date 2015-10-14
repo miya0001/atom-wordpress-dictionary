975255207cc523a3824f084be0633aad7ffe1471
miya0001 <miya@wpist.me>
2015-10-15 04:03:14 +0900

    cson

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,14 @@
 
 var gulp = require( "gulp" );
 var fs = require( "fs" );
+var cson = require('CSON');
 var sprintf = require("sprintf-js").sprintf;
 
 gulp.task( 'functions', function() {
     var src = './node_modules/vim-dict-wordpress/functions.dict';
     var out = './snippets/functions.cson';
-    var snippets = "'.source.php':\n"
+    var snippets = {};
+    snippets['.source.php'] = {};
     fs.readFile( src, 'utf8', function ( err, text ) {
         var lines = text.split( /\n/ );
         for ( var i = 0; i < lines.length; i++ ) {
@@ -15,18 +17,22 @@ gulp.task( 'functions', function() {
             if ( ! snippet.length ) {
                 continue;
             }
-            snippets = snippets + "  " + "'function: " + snippet + "()':\n";
-            snippets = snippets + "    " + "'prefix': '" + snippet + "()'\n";
-            snippets = snippets + "    " + "'body': '" + snippet + "(${1})'\n";
+            snippets['.source.php']['function: ' + snippet + '()'] = {
+                'prefix': snippet,
+                'body': snippet + '(${1})',
+            }
         }
-        fs.writeFile( out, snippets );
+        cson.createCSONString( snippets, function( err,result ){
+            fs.writeFile( out, result );
+        })
     } );
 } );
 
 gulp.task( 'filters', function() {
     var src = './node_modules/vim-dict-wordpress/filter-hooks.dict';
     var out = './snippets/filters.cson';
-    var snippets = "'.source.php':\n"
+    var snippets = {};
+    snippets['.source.php'] = {};
     fs.readFile( src, 'utf8', function ( err, text ) {
         var lines = text.split( /\n/ );
         for ( var i = 0; i < lines.length; i++ ) {
@@ -34,18 +40,22 @@ gulp.task( 'filters', function() {
             if ( ! snippet.length ) {
                 continue;
             }
-            snippets += sprintf( "  'filter: %s':\n", snippet );
-            snippets += sprintf( "    'prefix': '%s'\n", snippet );
-            snippets += sprintf( "    'body': 'add_filter( \"%1$s\", ${1:array( $this, \"%1$s\" )} );'\n", snippet );
+            snippets['.source.php']['filter: ' + snippet + '()'] = {
+                'prefix': snippet,
+                'body': sprintf( 'add_filter( "%1$s", ${1:array( $this, "%1$s" )} );', snippet ),
+            }
         }
-        fs.writeFile( out, snippets );
+        cson.createCSONString( snippets, function( err,result ){
+            fs.writeFile( out, result );
+        })
     } );
 } );
 
 gulp.task( 'actions', function() {
     var src = './node_modules/vim-dict-wordpress/action-hooks.dict';
     var out = './snippets/actions.cson';
-    var snippets = "'.source.php':\n"
+    var snippets = {};
+    snippets['.source.php'] = {};
     fs.readFile( src, 'utf8', function ( err, text ) {
         var lines = text.split( /\n/ );
         for ( var i = 0; i < lines.length; i++ ) {
@@ -53,11 +63,14 @@ gulp.task( 'actions', function() {
             if ( ! snippet.length ) {
                 continue;
             }
-            snippets += sprintf( "  'action: %s':\n", snippet );
-            snippets += sprintf( "    'prefix': '%s'\n", snippet );
-            snippets += sprintf( "    'body': 'add_action( \"%1$s\", ${1:array( $this, \"%1$s\" )} );'\n", snippet );
+            snippets['.source.php']['action: ' + snippet + '()'] = {
+                'prefix': snippet,
+                'body': sprintf( 'add_action( "%1$s", ${1:array( $this, "%1$s" )} );', snippet ),
+            }
         }
-        fs.writeFile( out, snippets );
+        cson.createCSONString( snippets, function( err,result ){
+            fs.writeFile( out, result );
+        })
     } );
 } );
 
